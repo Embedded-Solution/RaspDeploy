823da6c6d1f9ce6bc007d0981a134c990d6d16b6
jld <jeanluc.damour@free.fr>
2019-09-06 09:25:12 +0200

    Création des utilisateurs 'edk' par défaut Management du plugin "User-Agent Switcher for Chrome" (création de la configuration pour les DRM)

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,7 @@
 # Fichier de déploiement d'une interface Kiosk                   #
 # à partir d'une image Raspbian 10                               #
 #                                                                #
-#                                                                #
 ##################################################################
-
-VERSION="master"
-KUSER="pi"
 
 # parser les arguments
 while getopts v:c option; do
@@ -20,15 +16,27 @@ while getopts v:c option; do
 	esac
 done
 
+VERSION="master"
+KUSER="edkuser"
+GRPSADMIN="adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,spi,i2c,gpio"
+GRPSREST="dialout,cdrom,audio,video,games,users,input,netdev,gpio"
+INFOS=$(curl http://deploy.ioconstellation.com/infos/infos.txt)
+eval $INFOS
+
+echo "Version = " $VERSION
+
 if [ -n $CLEAN ]; then
 	echo "Tout Nettoyer"
 fi
 
-echo "Version = " $VERSION
+############## PAQUETS, BIBLIOTEQUE, ETC, #####################
 
 # Mise à jour 
 apt update
 apt upgrade -y
+
+# Installation des paquest utiles
+apt install accountsservice unclutter matchbox thunderbird thunderbird-l10n-fr -y
 
 # Suppression des paquets inutiles
 apt remove geany geany-common -y
@@ -41,33 +49,55 @@ if ! dpkg -s anydesk >/dev/null 2>&1; then
     rm anydesk_5.1.1-1_armhf.deb
 fi
 
-# Installation des jeux
-apt install scummvm -y
 
-#enlever la souris de l'écran
-apt install unclutter -y
+########## SCRIPTS ET MODIFICATION SYSTEM (LOOK, ETC...)
 
-#installer un clavier
-apt install matchbox -y
+# Installation des scripts utilisateurs et système
+cp -f ./alluserscripts/* /usr/local/sbin
 
-#installtion de libwidevine binary and extract pour useragent
+# Copie des fichier partagés par ts les utilisateurs
+cp -Rf ./usersshare/* /usr/share
+
+
+########## CRÉATION ET CONFIGURATION DES UTILISATEURS ###########
+
+# Créations des utilisateurs
+if ! id edkuser > /dev/null 2>&1; then
+  echo "création de l'utilisateur edkuser"
+  useradd -d /home/edkuser -G $GRPSREST -m -p $(echo $EDKPW | openssl passwd -1 -stdin) edkuser
+fi
+if ! id edkstf > /dev/null 2>&1; then
+  echo "création de l'utilisateur edkstf"
+  useradd -d /home/edkstf -G $GRPSADMIN -m -p $(echo $EDKSPW | openssl passwd -1 -stdin) edkstf
+fi
+
+# Modifier les droits sudoer
+echo "edkstf ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/020_edkstf-nopasswd
+echo "edkuser ALL=(ALL) NOPASSWD: /sbin/reboot,/sbin/shutdown,/usr/sbin/service" > /etc/sudoers.d/02_edkuser-nopasswd
+
+# Changer l'utilisateur "par defaut"
+sudo sed -i "s/autologin-user=pi/autologin-user=edkuser/g" /etc/lightdm/lightdm.conf
+
+# Copier les fichiers/dossiers home par defaut des utilisateurs
+for luser in pi edkuser edkstf
+do 
+	cp -Rf ./alluserhome -T /home/$luser
+	chown -R $luser:$luser /home/$luser
+	#chmod +x /home/$luser/.script/*
+done
+
+
+##################### CHROMIUM #######################
+
+#installtion de libwidevine pour lecture des drm
 cp ./chromium/libwidevinecdm.so /usr/lib/chromium-browser
 
-#Messagerie Thunderbird pour travailler en off line
-apt install thunderbird thunderbird-l10n-fr -y
-
 # Installation des plugins Chromium
-for f in ./chromium/plugins/*.json; do
+for f in ./chromium/policies/*.json; do
     # do some stuff here with "$f"
     echo 'Installation des plugins ' $f
     cp $f  /etc/chromium-browser/policies/managed
 done
-
-# Copier les fichiers/dossiers de /pi / dans /home/$KUSER
-cp -Rfv ./$KUSER /home
-chown -R $KUSER:$KUSER /home/$KUSER
-chmod +x /home/$KUSER/.script/*
-
 
 # Installation et configuration de supervisor
 apt install supervisor -y
@@ -76,25 +106,19 @@ for f in ./supervisor/*.conf; do
     cp $f  /etc/supervisor/conf.d/
 done
 
-# Installation de Iomanage
-cp -f ./iomanage/iomanage.py /usr/local/sbin/
-cp -f ./iomanage/logmanage.py /usr/local/sbin/
-
 # Installer Flaskinterface
 apt install python3-flask python3-flask-sqlalchemy -y
 git clone -b $VERSION http://deploy.ioconstellation.com/iostaff/flaskinterface.git
 rm -R flaskinterface/.git
-cp -Rfv flaskinterface /opt
+cp -Rf flaskinterface /opt
 chown -R $KUSER:$KUSER /opt/flaskinterface
 
-# Modification du look du bureau
-cp -Rfv ./raspberrypi-artwork /usr/share
-cp -fv ./divers/temple.jpg /usr/share/rpd-wallpaper/temple.jpg
+
 ### à faire: changer framboise ###########################
 
 # Copier les fichiers de boot et de splash
-cp -Rfv ./boot /
-cp -fv ./divers/splash.png /usr/share/plymouth/themes/pix/splash.png
+#cp -Rfv ./boot /
+#cp -fv ./divers/splash.png /usr/share/plymouth/themes/pix/splash.png
 
 
 # Nettoyer le cache apt
